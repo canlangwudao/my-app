ded682b85151a5734d62541c54d167924cd9d667
wanggangcanlang@sina.com <wanggangcanlang@sina.com>
2017-10-31 15:03:52 +0800

    路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,15 @@ import { CarouselComponent } from './carousel/carousel.component';
 import { ProductComponent } from './product/product.component';
 import { StarsComponent } from './stars/stars.component';
 import { RightbarComponent } from './rightbar/rightbar.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './home/home.component';
+import {RouterModule, Routes} from '@angular/router';
+
+const routeConfig: Routes = [
+  { path: '' , component: HomeComponent},
+  { path: 'product/:title', component: ProductDetailComponent},
+];
+
 
 @NgModule({
   declarations: [
@@ -19,10 +28,13 @@ import { RightbarComponent } from './rightbar/rightbar.component';
     CarouselComponent,
     ProductComponent,
     StarsComponent,
-    RightbarComponent
+    RightbarComponent,
+    ProductDetailComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routeConfig),
   ],
   providers: [],
   bootstrap: [AppComponent]
